6449a7a115fa6358d48fd89ec074e582ff9b65c0
Ibrahem Wattaweda <wattad111@gmail.com>
2021-01-24 12:15:42 +0200

    b5

--- MyCinema/Movy.cs
+++ MyCinema/Movy.cs
@@ -11,13 +11,23 @@ namespace MyCinema
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class Movy
     {
         public int Id { get; set; }
         public string movie_name { get; set; }
         public string movie_dis { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        [Display(Name = "Movie's Date")]
+        [DataType(DataType.Date)]
+        // [DisplayFormat(DataFormatString ="{0:Day/Mont/year}",ApplyFormatInEditMode =true)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public System.DateTime movie_date { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        [DataType(DataType.Time)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:H:mm}")]
+        [Display(Name = "Movie's Time")]
         public string movie_time { get; set; }
         public string movie_pic { get; set; }
         public string movie_hall { get; set; }
